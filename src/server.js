c9b99fa57df4357c1ae9cd7dd3fb8671fdcba417
Ariun <thiagotatico007@gmail.com>
2022-02-18 14:21:40 -0300

    organizing folders

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const server = express()
 
-server.listen(3000, () => console.log('Server ON'))
+// Enable static files
+server.use(express.static('public')) // 'Use' is for add functionalities in the server
 
 server.get('/', (request, response) => {
   return response.sendFile(__dirname + '/views/index.html')
 })
+
+server.listen(3000, () => console.log('Server ON'))
