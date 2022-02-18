e8a232af7b9f2a51d2338f92518a202921d54bb7
Ariun <thiagotatico007@gmail.com>
2022-02-18 14:45:34 -0300

    create  routes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const server = express()
+const routes = require('./routes')
 
 // Enable static files
 server.use(express.static('public')) // 'Use' is for add functionalities in the server
 
-server.get('/', (request, response) => {
-  return response.sendFile(__dirname + '/views/index.html')
-})
+// Routes
+server.use(routes)
+
+
+
+
+
+
 
 server.listen(3000, () => console.log('Server ON'))
