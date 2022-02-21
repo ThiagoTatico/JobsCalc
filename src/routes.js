5556a0ef60467d1cfe4e7c1406e596a37d249d23
Ariun <thiagotatico007@gmail.com>
2022-02-21 20:54:40 -0300

    Change rotes

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,8 @@ const profile = {
   'monthly-budget': 3000,
   'days-per-week': 5,
   'hours-per-day': 5,
-  'vacation-per-year': 4
+  'vacation-per-year': 4,
+  'value-hour': 75
 }
 
 const jobs = [
@@ -29,23 +30,38 @@ const jobs = [
   }
 ]
 
+function remainingDays(job) {
+  const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+  const createdDate = new Date(job.createdAt)
+  const dueDay = createdDate.getDate() + Number(remainingDays)
+  const dueDateInMs = createdDate.setDate(dueDay)
+
+  const timeDiffInMs = dueDateInMs - Date.now()
+
+  //Transforming Mile seconds in Days
+  const dayInMs = 1000 * 60 * 60 * 24
+  const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+  // remain X days
+  return dayDiff
+}
+
 routes.get('/', (req, res) => {
+
   const updatedJobs = jobs.map((job) => {
-    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+    const remaining = remainingDays(job)
+    const status = remaining <= 0 ? 'done' : 'progress'
 
-    const createdDate = new Date(job.createdAt)
-    const dueDay = createdDate.getDate() + Number(remainingDays)
-    const dueDateInMs = createdDate.setDate(dueDay)
-
-    const timeDiffInMs = dueDateInMs - Date.now()
-
-    //Transforming Mile seconds in Days
-    const dayInMs = 1000 * 60 * 60 * 24
-
-    return job
+    return {
+      ...job,
+      remaining,
+      status,
+      budget: profile['value-hour'] * job['total-hours']
+    }
   })
 
-  return res.render(views + 'index', {jobs})
+  return res.render(views + 'index', {jobs: updatedJobs})
 })
 
 routes.get('/job', (req, res) => res.render(views + 'job'))
