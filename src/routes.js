7cdbeeec7d8810dced4ad9d78902891f0366aa12
Ariun <thiagotatico007@gmail.com>
2022-02-23 19:26:09 -0300

    refactoring

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,21 @@ const Profile = {
     },
 
     update(req, res) {
-      
+      const data = req.body
+      const weeksPerYear = 52
+      const weeksPerMonth = (weeksPerYear - data['vacation-per-year']) / 12
+      const weekTotalHours = data['hours-per-day'] * data['days-per-week']
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+      const valueHour = data['value-hour'] = data['monthly-budget'] / monthlyTotalHours
+
+      Profile.data = {
+        ...Profile.data,
+        ...req.body,
+        'value-hour': valueHour
+      }
+
+      return res.redirect('/profile')
     }
   }
 }
