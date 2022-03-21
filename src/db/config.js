48f87041d6b4c9572b9872fc50c81f1f84b47b73
Ariun <thiagotatico007@gmail.com>
2022-03-21 10:01:05 -0300

    Simple refactoring and initiate DB creation

--- src/db/config.js
+++ src/db/config.js
@@ -1,3 +1,9 @@
 const sqlite3 = require('sqlite3')
 const { open } = require('sqlite') // get only 'open' function in sqlite
 
+module.exports = () => {  // The 'open' needs to be inside a function structure
+  open({
+    filename: './database.sqlite',
+    driver: sqlite3.Database
+  })
+}
