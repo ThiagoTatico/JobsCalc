110ed755dfa022cff09e3a26e3a205794dd48064
Ariun <thiagotatico007@gmail.com>
2022-03-21 16:59:32 -0300

    initial configs for DB

--- src/db/init.js
+++ src/db/init.js
@@ -1,42 +1,75 @@
-const Database = require('config')
+const Database = require('./config')
 
-Database()
+const initDb = {
 
-Database.exec(`CREATE TABLE profile (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  avatar TEXT,
-  monthly_budget INT,
-  days_per_week INT,
-  hours_per_day INT,
-  vacation_per_year INT,
-  value_hour INT
-)`)
+  async init(){
 
-Database.exec(`CREATE TABLE jobs (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  daily_hours INT,
-  total_hours INT,
-  created_at DATETIME
-)`)
+    // "async" "await" is used for wait the connection between the JS and Sqlite
+    const db = await Database()
 
-Database.run(`INSERT INTO profile (
-  name,
-  avatar,
-  monthly_budget,
-  days_per_week,
-  hours_per_day,
-  vacation_per_year,
-  value_hour
-) VALUES (
-  "Thiago Tatico",
-  "https://avatars.githubusercontent.com/u/86478479?v=4/",
-  3000,
-  5,
-  5,
-  4,
-  75
-)`)
+    await db.exec(`CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    avatar TEXT,
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
+    )`);
 
-Database.close()
+    await db.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME
+    )`);
+
+    await db.run(`INSERT INTO profile (
+    name,
+    avatar,
+    monthly_budget,
+    days_per_week,
+    hours_per_day,
+    vacation_per_year,
+    value_hour
+    ) VALUES (
+    "Thiago Tatico",
+    "https://avatars.githubusercontent.com/u/86478479?v=4/",
+    3000,
+    5,
+    5,
+    4,
+    75
+    )`);
+
+    await db.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+    ) VALUES (
+    "Pizzaria Guloso",
+    2,
+    1,
+    1617514376018
+    )`);
+
+    await db.run(`INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+    ) VALUES (
+    "OneTwo Projects",
+    3,
+    47,
+    1617514376018
+    )`);
+
+    await db.close();
+  }
+}
+
+initDb.init();
